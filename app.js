1121d68946f21c7fdc97b7ddf7cadc1423f1b746
MPL S3 <mpl.s3@mizigsolutions.com>
2024-11-22 23:01:58 +0530

    Global Variable learnt

---
+++ app.js
@@ -0,0 +1,11 @@
+// Global Variables
+
+// __dirname Global Variable
+console.log(__dirname);
+
+// __filename Global Variable
+console.log(__filename);
+
+// Define a global variable in NodeJS
+global.myVariable = "Hello World!";
+console.log(myVariable);
