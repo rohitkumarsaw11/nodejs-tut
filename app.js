41d5a0058a94a823a91fec0fc99cd29ed32a8c4a
MPL S3 <mpl.s3@mizigsolutions.com>
2024-11-23 17:02:33 +0530

    The HTTP Module: Different content-type serving

--- app.js
+++ app.js
@@ -1,32 +1,87 @@
 // The HTTP Module
-
+const fs = require('fs');
 const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   switch (url) {
+//     case "/":
+//       res.writeHead(200, {'content-type': 'text/plain'});
+//       res.write('<h1>Home Page</h1>');
+//       res.end();
+//       break;
+//     case "/about":
+//       res.writeHead(200, {'content-type': 'text/html'});
+//       res.write('<h1>About Page</h1>');
+//       res.end();
+//       break;
+//     case "/contact":
+//       res.writeHead(200, {'content-type': 'text/html'});
+//       res.write('<h1>Contact Page</h1>');
+//       res.end();
+//       break;
+//     default:
+//       res.writeHead(404, {'content-type': 'text/html'});
+//       res.write('<h1>404, Resource Not Found</h1><br/><a href="/">Go Back Home</a>');
+//       res.end();
+//   }
+// });
+
+const homePage = fs.readFileSync('./navbar-app/index.html');
+const homeStyles = fs.readFileSync('./navbar-app/styles.css');
+const homeLogo = fs.readFileSync('./navbar-app/logo.svg');
+const homeLogic = fs.readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  console.log('Requested URL: ' + url);
+
   switch (url) {
     case "/":
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('<h1>Home Page</h1>');
+      res.writeHead(200, {'content-type': 'text/html'});
+      res.write(homePage);
       res.end();
       break;
+
+    case "/styles.css":
+      res.writeHead(200, {'content-type': 'text/css'});
+      res.write(homeStyles);
+      res.end();
+      break;
+
+    case "/browser-app.js":
+      res.writeHead(200, {'content-type': 'text/javascript'});
+      res.write(homeLogic);
+      res.end();
+      break;
+
+    case "/logo.svg":
+      res.writeHead(200, {'content-type': 'image/svg+xml'});
+      res.write(homeLogo);
+      res.end();
+      break;
+
     case "/about":
       res.writeHead(200, {'content-type': 'text/html'});
       res.write('<h1>About Page</h1>');
       res.end();
       break;
+
     case "/contact":
       res.writeHead(200, {'content-type': 'text/html'});
       res.write('<h1>Contact Page</h1>');
       res.end();
       break;
+  
     default:
       res.writeHead(404, {'content-type': 'text/html'});
-      res.write('<h1>404, Resource Not Found</h1><br/><a href="/">Go Back Home</a>');
+      res.write('<h1>404! Resource not found.</h1>');
       res.end();
   }
-});
 
-server.listen(5000, () => {
-  console.log('Server listening at port 5000');
+})
+
+const port = 5001;
+server.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
 })
