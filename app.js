280917852683d072984a186137393b5ef13c472c
MPL S3 <mpl.s3@mizigsolutions.com>
2024-11-23 16:41:27 +0530

    The HTTP Module

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-// Event-Driven Programming
+// The HTTP Module
 
-const EventEmitter = require('events');
+const http = require('http');
 
-const myEmitter = new EventEmitter();
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  switch (url) {
+    case "/":
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('<h1>Home Page</h1>');
+      res.end();
+      break;
+    case "/about":
+      res.writeHead(200, {'content-type': 'text/html'});
+      res.write('<h1>About Page</h1>');
+      res.end();
+      break;
+    case "/contact":
+      res.writeHead(200, {'content-type': 'text/html'});
+      res.write('<h1>Contact Page</h1>');
+      res.end();
+      break;
+    default:
+      res.writeHead(404, {'content-type': 'text/html'});
+      res.write('<h1>404, Resource Not Found</h1><br/><a href="/">Go Back Home</a>');
+      res.end();
+  }
+});
 
-// const sayHello = () => {
-//   console.log("Hello User");
-// }
-
-// const sayHi = () => {
-//   console.log("Hi User");
-// }
-
-// const greetNewYear = () => {
-//   console.log("Happy New Year");
-// }
-
-
-// myEmitter.on("userJoined", sayHello);
-// myEmitter.on("userJoined", sayHi);
-// myEmitter.on("userJoined", greetNewYear);
-
-// myEmitter.emit('userJoined');
-
-const greetBirthday = (name, newAge) => {
-  console.log(`Happy Birthday ${name}. You are now ${newAge}`);
-}
-
-myEmitter.on('birthdayEvent', greetBirthday);
-
-// Emitting the birthdayEvent with some extra parameters
-myEmitter.emit('birthdayEvent', 'Rohit', 26);
+server.listen(5000, () => {
+  console.log('Server listening at port 5000');
+})
